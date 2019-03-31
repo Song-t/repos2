25c521f1f4fb396f15be42e99e14774dfa8805c8
axiba <893364516@qq.com>
2019-03-31 11:44:47 +0800

    修改hello

--- idea-git-test/src/com/heima/Hello.java
+++ idea-git-test/src/com/heima/Hello.java
@@ -4,6 +4,16 @@ public class Hello {
     public static void main(String[] args) {
         int i = 10;
         i += 10;
-        System.out.println(123);
+        System.out.println(i);
+        test();
+    }
+
+    public static void test() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

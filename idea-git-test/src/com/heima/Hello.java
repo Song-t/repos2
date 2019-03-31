38448696f084a81b5cf3a9a6ce4eab281b899dfd
axiba <893364516@qq.com>
2019-03-31 14:39:50 +0800

    修改hello

--- idea-git-test/src/com/heima/Hello.java
+++ idea-git-test/src/com/heima/Hello.java
@@ -1,12 +1,24 @@
 package com.heima;
 
+import java.util.Arrays;
+
 public class Hello {
     public static void main(String[] args) {
         int i = 10;
         i += 10;
         System.out.println(i);
         test();
-        System.out.println(11111);
+        for (int j = 0; j < 5; j++) {
+            test();
+        }
+
+        int[] arr = new int[5];
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = j;
+
+        }
+        String str = Arrays.toString(arr);
+        System.out.println(str);
     }
 
     public static void test() {
